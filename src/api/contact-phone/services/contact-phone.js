91e661cf347eda21d87e0c6b6b53671341c8a09b
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2023-09-15 15:21:00 -0500

    fix: docker-compose production added and docker-compose development fixed

---
+++ src/api/contact-phone/services/contact-phone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-phone service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contact-phone.contact-phone');
